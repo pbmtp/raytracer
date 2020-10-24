75e377e6836ba0213f40b99eaa6c9cb0dc091b14
Damien Deville <da.deville@gmail.com>
2020-10-24 23:42:39 +0200

    add sphere type.

---
+++ src/sphere.rs
@@ -0,0 +1,51 @@
+use super::hittable::{HitRecord, Hittable};
+use super::ray::Ray;
+use super::vec3::{Point3, Vec3};
+
+#[derive(Debug)]
+pub struct Sphere {
+    center: Point3,
+    radius: f32,
+}
+
+impl Sphere {
+    pub fn new(center: Point3, radius: f32) -> Sphere {
+        Sphere { center, radius }
+    }
+}
+
+impl Hittable for Sphere {
+    fn hit(&self, r: &Ray, tmin: f32, tmax: f32) -> Option<HitRecord> {
+        let oc: Vec3 = r.origin() - self.center;
+        let a = r.direction().length_squared();
+        let half_b = oc.dot(r.direction());
+        let c = oc.length_squared() - self.radius.powi(2);
+        let discriminant = half_b.powi(2) - a * c;
+
+        if discriminant > 0.0 {
+            let root = discriminant.sqrt();
+
+            let temp = (-half_b - root) / a;
+            if temp > tmin && temp < tmax {
+                let p = r.point_at(temp);
+                let outward_normal = (p - self.center) / self.radius;
+
+                let hr = HitRecord::new(p, Vec3::zero(), temp);
+                hr.set_front_face(&r, outward_normal);
+                return Some(hr);
+            }
+
+            let temp = (-half_b + root) / a;
+            if temp > tmin && temp < tmax {
+                let p = r.point_at(temp);
+                let outward_normal = (p - self.center) / self.radius;
+
+                let hr = HitRecord::new(p, Vec3::zero(), temp);
+                hr.set_front_face(&r, outward_normal);
+                return Some(hr);
+            }
+        }
+
+        None
+    }
+}
