8f0d0986ee38bce42542ee117ba2dafd3bcccc2d
Damien Deville <da.deville@gmail.com>
2020-10-24 17:29:34 +0200

    add basic ray struct.

---
+++ src/ray.rs
@@ -0,0 +1,18 @@
+use super::vec3::{Point3, Vec3};
+pub struct Ray {
+    origin: Point3,
+    direction: Vec3,
+}
+
+impl Ray {
+    pub fn new(o: Point3, d: Vec3) -> Ray {
+        Ray {
+            origin: o,
+            direction: d,
+        }
+    }
+
+    pub fn point_at(self, t: f32) -> Vec3 {
+        self.origin + (self.direction * t)
+    }
+}
