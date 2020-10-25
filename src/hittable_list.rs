9a8b8aa531a55960ca4c5bbaffbefc000ef7b43c
Damien Deville <da.deville@gmail.com>
2020-10-25 12:14:12 +0100

    add list of objects abstraction. update main to use world list of object. finish chapter 6.

---
+++ src/hittable_list.rs
@@ -0,0 +1,45 @@
+use super::hittable::{HitRecord, Hittable};
+use super::ray::Ray;
+use super::vec3::Vec3;
+
+#[derive(Default)]
+pub struct HittableList<T> {
+    pub objects: Vec<T>,
+}
+
+impl<T: Hittable> HittableList<T> {
+    pub fn new(object: T) -> HittableList<T> {
+        HittableList::<T> {
+            objects: vec![object],
+        }
+    }
+
+    pub fn clear(&mut self) {
+        self.objects.clear();
+    }
+
+    pub fn add(&mut self, object: T) {
+        self.objects.push(object);
+    }
+}
+
+impl<T: Hittable> Hittable for HittableList<T> {
+    fn hit(&self, r: &Ray, tmin: f32, tmax: f32) -> Option<HitRecord> {
+        let mut rec = HitRecord::new(Vec3::zero(), Vec3::zero(), 0.0);
+        let mut hit_anything = false;
+        let mut closest_so_far = tmax;
+
+        for obj in self.objects.iter() {
+            if let Some(hr) = obj.hit(r, tmin, closest_so_far) {
+                hit_anything = true;
+                closest_so_far = hr.get_t();
+                rec = hr;
+            }
+        }
+        if hit_anything {
+            Some(rec)
+        } else {
+            None
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,21 @@
 extern crate image;
 extern crate indicatif;
 
+// use std::f32::consts::PI;
+
 use image::{ImageBuffer, Rgb};
 use indicatif::ProgressBar;
 
 mod hittable;
+mod hittable_list;
 mod ray;
 mod sphere;
 mod vec3;
 
+use hittable::Hittable;
+use hittable_list::HittableList;
 use ray::Ray;
+use sphere::Sphere;
 use vec3::{Color, Point3, Vec3};
 
 // Size
@@ -56,7 +62,7 @@ fn hit_sphere(center: Point3, radius: f32, r: &Ray) -> f32 {
     }
 }
 
-fn ray_color(r: Ray) -> Color {
+fn ray_color_one_sphere(r: Ray) -> Color {
     let mut t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, &r);
     if t > 0.0f32 {
         let v = r.point_at(t) - Vec3::new(0.0, 0.0, -1.0);
@@ -71,7 +77,7 @@ fn ray_color(r: Ray) -> Color {
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
 }
 
-fn render_with_ray(name: &str) {
+fn render_with_ray_one_sphere(name: &str) {
     // Camera
     let viewport_height = 2.0f32;
     let viewport_width = RATIO * viewport_height;
@@ -99,7 +105,64 @@ fn render_with_ray(name: &str) {
             lower_left_corner + u * horizontal + v * vertical - origin,
         );
 
-        let c = ray_color(r);
+        let c = ray_color_one_sphere(r);
+
+        *pixel = Rgb(c.to_u8());
+    }
+    bar.finish();
+
+    // write the generated image (format is deduced based on extension)
+    imgbuf.save(name).unwrap();
+}
+
+fn ray_color_world<T: Hittable>(r: &Ray, world: &T) -> Color {
+    if let Some(hr) = world.hit(&r, 0.0, std::f32::INFINITY) {
+        return 0.5 * (hr.get_normal() + Color::new(1.0, 1.0, 1.0));
+    }
+
+    let unit_direction = r.direction().to_unit_vector();
+    let t = 0.5 * (unit_direction.y() + 1.0);
+
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
+
+fn render_world(name: &str) {
+    // World
+    let mut world = HittableList {
+        objects: Vec::new(),
+    };
+
+    world.add(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5));
+    world.add(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0));
+
+    // Camera
+    let viewport_height = 2.0f32;
+    let viewport_width = RATIO * viewport_height;
+    let focal_length = 1.0f32;
+
+    let origin = Point3::zero();
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+
+    // create image buffer
+    let mut imgbuf = ImageBuffer::new(WIDTH, HEIGHT);
+
+    // Iterate over the coordinates and pixels of the image
+    let len = WIDTH as u64 * HEIGHT as u64;
+    let bar = ProgressBar::new(len);
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        bar.inc(1);
+
+        let u = x as f32 / (WIDTH as f32 - 1f32);
+        let v = (HEIGHT - y) as f32 / (HEIGHT as f32 - 1f32);
+        let r = Ray::new(
+            origin,
+            lower_left_corner + u * horizontal + v * vertical - origin,
+        );
+
+        let c = ray_color_world(&r, &world);
 
         *pixel = Rgb(c.to_u8());
     }
@@ -110,6 +173,7 @@ fn render_with_ray(name: &str) {
 }
 
 fn main() {
-    // render_simple("out.png");
-    render_with_ray("out.png");
+    // render_simple("out-ch2.png");
+    // render_with_ray_one_sphere("out-ch6_1.png");
+    render_world("out-ch6_6.png");
 }
