1b4d5360d8072d5d58c0e4b786bf0d907cae3791
Damien Deville <da.deville@gmail.com>
2020-10-29 21:31:21 +0100

    use rayon and parallel iter.

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,11 @@
 extern crate image;
 extern crate indicatif;
 extern crate rand;
+extern crate rayon;
 
 use image::{ImageBuffer, Rgb};
 use indicatif::{HumanDuration, ProgressBar};
+use rayon::prelude::*;
 
 // use std::f64::consts::PI;
 use std::time::Instant;
@@ -43,12 +45,13 @@ const MAX_DEPTH: u32 = 50;
 */
 
 // Best quality
+/**/
 const RATIO: f64 = 3.0 / 2.0;
 const WIDTH: u32 = 1200;
 const HEIGHT: u32 = (WIDTH as f64 / RATIO) as u32;
 const SAMPLES_PER_PIXEL: u32 = 500;
 const MAX_DEPTH: u32 = 50;
-
+/**/
 
 fn ray_color<T: Hittable>(r: &Ray, world: &T, depth: u32) -> Color {
     // If we've exceeded the ray bounce limit, no more light is gathered.
@@ -71,7 +74,7 @@ fn ray_color<T: Hittable>(r: &Ray, world: &T, depth: u32) -> Color {
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
 }
 
-fn random_scene() -> HittableList<Sphere> {
+fn random_scene(randomize: bool) -> HittableList<Sphere> {
     // World
     let mut world = HittableList {
         objects: Vec::new(),
@@ -86,45 +89,47 @@ fn random_scene() -> HittableList<Sphere> {
     });
 
     // random part
-    for a in -11..11 {
-        for b in -11..11 {
-            let choose_mat = random_double();
-            let center = Point3::new(
-                a as f64 + 0.9 * random_double(),
-                0.2,
-                b as f64 + 0.9 * random_double(),
-            );
+    if randomize {
+        for a in -11..11 {
+            for b in -11..11 {
+                let choose_mat = random_double();
+                let center = Point3::new(
+                    a as f64 + 0.9 * random_double(),
+                    0.2,
+                    b as f64 + 0.9 * random_double(),
+                );
 
-            if (center - Point3::new(4.0, 0.2, 0.0)).length() > 0.9 {
-                // shared_ptr<material> sphere_material;
+                if (center - Point3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                    // shared_ptr<material> sphere_material;
 
-                if choose_mat < 0.8 {
-                    // diffuse
-                    let albedo = Color::random() * Color::random();
-                    let sphere_material = Lambertian::new(albedo);
-                    world.add(Sphere {
-                        center,
-                        radius: 0.2,
-                        material: Box::new(sphere_material),
-                    });
-                } else if choose_mat < 0.95 {
-                    // metal
-                    let albedo = Color::random_range(0.5, 1.0);
-                    let fuzz = random_double_range(0.0, 0.5);
-                    let sphere_material = Metal::new(albedo, fuzz);
-                    world.add(Sphere {
-                        center,
-                        radius: 0.2,
-                        material: Box::new(sphere_material),
-                    });
-                } else {
-                    // glass
-                    let sphere_material = Dielectric::new(1.5);
-                    world.add(Sphere {
-                        center,
-                        radius: 0.2,
-                        material: Box::new(sphere_material),
-                    });
+                    if choose_mat < 0.8 {
+                        // diffuse
+                        let albedo = Color::random() * Color::random();
+                        let sphere_material = Lambertian::new(albedo);
+                        world.add(Sphere {
+                            center,
+                            radius: 0.2,
+                            material: Box::new(sphere_material),
+                        });
+                    } else if choose_mat < 0.95 {
+                        // metal
+                        let albedo = Color::random_range(0.5, 1.0);
+                        let fuzz = random_double_range(0.0, 0.5);
+                        let sphere_material = Metal::new(albedo, fuzz);
+                        world.add(Sphere {
+                            center,
+                            radius: 0.2,
+                            material: Box::new(sphere_material),
+                        });
+                    } else {
+                        // glass
+                        let sphere_material = Dielectric::new(1.5);
+                        world.add(Sphere {
+                            center,
+                            radius: 0.2,
+                            material: Box::new(sphere_material),
+                        });
+                    }
                 }
             }
         }
@@ -157,7 +162,7 @@ fn random_scene() -> HittableList<Sphere> {
 
 fn render_world(name: &str) {
     // World
-    let world = random_scene();
+    let world = random_scene(true);
 
     // Camera
     let lookfrom = Point3::new(13.0, 2.0, 3.0);
@@ -177,15 +182,19 @@ fn render_world(name: &str) {
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
         bar.inc(1);
 
-        let mut c = Color::zero();
-        for _s in 0..SAMPLES_PER_PIXEL {
-            let u = (x as f64 + random_double()) / (WIDTH as f64 - 1f64);
-            let v = ((HEIGHT - y) as f64 + random_double()) / (HEIGHT as f64 - 1f64);
+        let c: Color = (0..SAMPLES_PER_PIXEL)
+            .into_par_iter()
+            .map(|_| {
+                let u = (x as f64 + random_double()) / (WIDTH as f64 - 1f64);
+                let v = ((HEIGHT - y) as f64 + random_double()) / (HEIGHT as f64 - 1f64);
 
-            let r = cam.get_ray(u, v);
+                let r = cam.get_ray(u, v);
 
-            c += ray_color(&r, &world, MAX_DEPTH);
-        }
+                ray_color(&r, &world, MAX_DEPTH)
+            })
+            .collect::<Vec<Color>>()
+            .iter()
+            .sum();
 
         *pixel = Rgb(c.to_u8_avg_gamma2(SAMPLES_PER_PIXEL));
     }
@@ -197,7 +206,7 @@ fn render_world(name: &str) {
 
 fn main() {
     let start = Instant::now();
-    render_world("out-ch13.png");
+    render_world("out-test.png");
     println!(
         "Time elapsed rendering  scene is: {}",
         HumanDuration(start.elapsed())
