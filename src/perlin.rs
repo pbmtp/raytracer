9baef695caded5713c0cf8bbba2740902e98416f
Damien Deville <da.deville@gmail.com>
2020-11-06 23:30:43 +0100

    Perlin with trilienear interpolation.

--- src/perlin.rs
+++ src/perlin.rs
@@ -21,11 +21,45 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: &Point3) -> f64 {
-        let i = (4.0 * p.x()) as isize & 255;
-        let j = (4.0 * p.y()) as isize & 255;
-        let k = (4.0 * p.z()) as isize & 255;
+        let u = p.x() - p.x().floor();
+        let v = p.y() - p.y().floor();
+        let w = p.z() - p.z().floor();
 
-        self.rand_vec[self.perm_x[i as usize] ^ self.perm_y[j as usize] ^ self.perm_z[k as usize]]
+        let i = p.x().floor() as isize;
+        let j = p.y().floor() as isize;
+        let k = p.z().floor() as isize;
+
+        let mut c = [[[0.0; 2]; 2]; 2];
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    let idx = (i + di) & 255;
+                    let jdx = (j + dj) & 255;
+                    let kdx = (k + dk) & 255;
+
+                    c[di as usize][dj as usize][dk as usize] =
+                        self.rand_vec[self.perm_x[idx  as usize] ^ self.perm_y[jdx  as usize] ^ self.perm_z[kdx  as usize]];
+                }
+            }
+        }
+
+        Perlin::trilinear_interpolation(&c, u, v, w)
+    }
+
+    fn trilinear_interpolation(c: &[[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+        let mut accum = 0.0;
+        for i in 0..2 {
+            for j in 0..2 {
+                for k in 0..2 {
+                    accum += (i as f64 * u + (1 - i) as f64 * (1.0 - u))
+                        * (j as f64 * v + (1 - j) as f64 * (1.0 - v))
+                        * (k as f64 * w + (1 - k) as f64 * (1.0 - w))
+                        * c[i][j][k];
+                }
+            }
+        }
+
+        accum
     }
 
     fn generate_rand_vec() -> Vec<f64> {
