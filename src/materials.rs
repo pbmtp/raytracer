12e1c4422dbbd37534a8b12a1e5e87b45b16e8f7
Damien Deville <da.deville@gmail.com>
2020-11-10 22:02:06 +0100

    add Isotropic material.

--- src/materials.rs
+++ src/materials.rs
@@ -151,3 +151,29 @@ impl Material for DiffuseLight {
         self.emit.value(u, v, &p)
     }
 }
+
+// Isotropic
+pub struct Isotropic {
+    albedo: Box<dyn Texture>,
+}
+
+impl From<Color> for Isotropic {
+    fn from(color: Color) -> Self {
+        Isotropic {
+            albedo: Box::new(SolidTexture::from(color)),
+        }
+    }
+}
+
+impl Material for Isotropic {
+    fn scatter(&self, ray: &Ray, hr: &HitRecord) -> Scatter {
+        Scatter {
+            attenuation: self.albedo.value(hr.get_u(), hr.get_v(), &hr.get_p()),
+            scattered: Some(Ray::new(
+                hr.get_p(),
+                Vec3::random_in_unit_sphere(),
+                ray.time(),
+            )),
+        }
+    }
+}
